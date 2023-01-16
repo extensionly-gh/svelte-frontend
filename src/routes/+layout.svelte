76464fb333a1fd136236839611190875cabfe244
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-16 15:14:11 -0300

    fix: prerender const

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,6 @@
 	import Navbar from './_Navbar.svelte';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import Footer from './_Footer.svelte';
-
-	export const prerender = true;
 </script>
 
 <div id="theme-container" data-theme="night" class="bg-base-300">
