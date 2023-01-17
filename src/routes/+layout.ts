5584edc31125c5b7c2070c289f373f7d29ad4d1c
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-16 23:46:32 -0300

    feat: prerender legal pages

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,10 +3,16 @@ import { browser } from '$app/environment'
 import { locale, waitLocale } from 'svelte-i18n'
 import { theme } from '$lib/stores'
 import type { LayoutLoad } from './$types'
+import { getThemeFromCookie } from '$lib/stores/theme.store'
 
 export const load: LayoutLoad = (async ({ data }) => {
 	if (browser) {
-		theme.set(data.theme || 'winter')
+		// data.theme will be undefined if the page is prerendered. In that case, we get the theme from the cookie.
+		if (!data.theme) {
+			theme.set(getThemeFromCookie() || 'winter')
+		} else {
+			theme.set(data.theme || 'winter')
+		}
 		locale.set(data.lang || window.navigator.language)
 
 		if (!data.lang) {
