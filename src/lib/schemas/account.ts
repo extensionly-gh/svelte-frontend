ed3151665abb62781750b40d898027723880f81e
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-20 17:56:47 -0300

    refactor: update schemas

--- src/lib/schemas/account.ts
+++ src/lib/schemas/account.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const userWithNameSchema = z.object({
-  name: z.string().max(255, { message: "validations.string.max" }),
+  name: z.string().min(3, 'validations.string.min').max(255, 'validations.string.max'),
 })
 
 export const userWithEmailSchema = z.object({
-  email: z.string().email({ message: "validations.email.invalid" }).max(255, { message: "validations.string.max" }),
+  email: z.string().email('validations.email.invalid'),
 })
