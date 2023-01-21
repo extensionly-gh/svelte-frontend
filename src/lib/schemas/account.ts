16f5509cb696d4e29e3d4ace84478175bec0af48
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-21 19:35:39 -0300

    feat: add strong password validations

--- src/lib/schemas/account.ts
+++ src/lib/schemas/account.ts
@@ -18,7 +18,10 @@ export const passwordUpdateSchema = z
 			.string()
 			.min(8, 'zod.password.min')
 			.max(255, 'zod.string.max')
-			.refine((v) => /[A-Z]/.test(v), 'zod.string.uppercase'),
+			.refine((v) => /[A-Z]/.test(v), 'zod.string.uppercase')
+			.refine((v) => /[a-z]/.test(v), 'zod.string.lowercase')
+			.refine((v) => /[0-9]/.test(v), 'zod.string.numeric')
+			.refine((v) => /[^A-Za-z0-9]/.test(v), 'zod.string.symbol'),
 		cpassword: z.string()
 	})
 	.superRefine(({ cpassword, password }, ctx) => {
