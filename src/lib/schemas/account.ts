23b2afdbac8d4107ff4d2acdf854453e63e3ccba
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-21 14:14:00 -0300

    refactor: finish i18n refactor

--- src/lib/schemas/account.ts
+++ src/lib/schemas/account.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const userUpdateSchema = z.object({
-	name: z.string().min(3, 'validations.string.min').max(255, 'validations.string.max'),
-	email: z.string().email('validations.email.invalid'),
-	phone: z.string().min(10, 'validations.string.min').max(20, 'validations.string.max').nullable().or(z.literal('')),
-	image: z.string().url('validations.url.invalid').nullable().or(z.literal('')),
+	name: z.string().min(3, 'zod.string.min').max(255, 'zod.string.max'),
+	email: z.string().email('zod.email.invalid'),
+	phone: z.string().min(10, 'zod.string.min').max(20, 'zod.string.max').nullable().or(z.literal('')),
+	image: z.string().url('zod.url.invalid').nullable().or(z.literal('')),
 });
