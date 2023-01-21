e4c2e9802a069d48594650e91d1849a2693d8c9b
Igor Dalepiane da Costa <igordalepiane@gmail.com>
2023-01-20 23:23:54 -0300

    sendInBlue config

--- src/lib/server/singletons.ts
+++ src/lib/server/singletons.ts
@@ -1,17 +1,28 @@
-import { env } from "$env/dynamic/private";
-import { PrismaClient } from "@prisma/client";
+import { env } from '$env/dynamic/private';
+import { PrismaClient } from '@prisma/client';
+//@ts-expect-error npm does not have types for this import
+import SibApiV3Sdk from 'sib-api-v3-sdk';
 
-const prismaGlobal = global as typeof global & {
-  prisma?: PrismaClient;
+const singletonsGlobal = global as typeof global & {
+	prisma?: PrismaClient;
+	sendInBlueApi?: any;
 };
 
 export const prisma: PrismaClient =
-  prismaGlobal.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-  });
+	singletonsGlobal.prisma ||
+	new PrismaClient({
+		log: env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+	});
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = env.SENDINBLUE_API_KEY;
+
+export const sendInBlueApi =
+	singletonsGlobal.sendInBlueApi || new SibApiV3Sdk.TransactionalEmailsApi();
 
 if (env.NODE_ENV !== 'production') {
-  prismaGlobal.prisma = prisma;
+	singletonsGlobal.prisma = prisma;
+	singletonsGlobal.sendInBlueApi = sendInBlueApi;
 }
