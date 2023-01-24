ae0a2d96c9f50fea06ca1b351b9fbbec6d232e30
Lucas Alexandre Fell <lucasafell@gmail.com>
2023-01-24 19:10:40 -0300

    feat: ✨ add system theme support (works with SSR)

--- src/lib/stores/theme.store.ts
+++ src/lib/stores/theme.store.ts
@@ -3,7 +3,33 @@ import { writable } from 'svelte/store';
 
 export type Theme = 'winter' | 'night' | undefined | null;
 
-export const theme = writable<Theme>('winter');
+export const theme = writable<Theme>('winter', (set) => {
+	if (!browser) return;
+
+	const preferredColorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+	preferredColorScheme.addEventListener('change', (e) => {
+		const useSystemTheme = document.cookie
+			.split('; ')
+			.find((row) => row.startsWith('useSystemTheme='))
+			?.split('=')[1] as string;
+
+		if (useSystemTheme === 'true') {
+			const theme = e.matches ? 'night' : 'winter';
+			console.log('theme', theme);
+
+			set(theme);
+			fetch('/api/theme', {
+				method: 'PUT',
+				body: JSON.stringify({ theme })
+			});
+		}
+	});
+
+	return () => {
+		preferredColorScheme.removeEventListener('change', (e) => set(e.matches ? 'night' : 'winter'));
+	};
+});
 
 export const getThemeFromCookie = (): Theme => {
 	if (browser) {
