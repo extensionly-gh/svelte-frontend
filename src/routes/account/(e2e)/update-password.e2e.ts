a979c690f30ebd7c90edfc10d39dece5319596ab
LucasFell <lucas.fell@partner.bmw.com.br>
2023-02-17 20:51:18 -0300

    test: ✅ update password

---
+++ src/routes/account/(e2e)/update-password.e2e.ts
@@ -0,0 +1,19 @@
+import test, { expect } from '@playwright/test';
+
+test('update password', async ({ page }) => {
+	await page.goto('/account', { waitUntil: 'networkidle' });
+	await page.getByTestId('currentPwd-input').fill('StrongPassword1.');
+	await page.getByTestId('newPwd-input').fill('#1Abcdef');
+	await page.getByTestId('confirmPwd-input').fill('#1Abcdef');
+	await page.getByTestId('updatepw-submit-button').click();
+	await expect(page.getByTestId('toast-body')).toHaveText('Password updated successfully!');
+});
+
+// Changes pw to the previous one
+test.afterAll(async ({ page }) => {
+	await page.goto('/account', { waitUntil: 'networkidle' });
+	await page.getByTestId('currentPwd-input').fill('#1Abcdef');
+	await page.getByTestId('newPwd-input').fill('StrongPassword1.');
+	await page.getByTestId('confirmPwd-input').fill('StrongPassword1.');
+	await page.getByTestId('updatepw-submit-button').click();
+});
