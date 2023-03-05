97484cb8c6355edfb8ad588e5ce0dadf7217ba09
LucasFell <lucas.fell@partner.bmw.com.br>
2023-03-05 09:31:55 -0300

    :recycle: (input): separate input styles to use with textarea

--- src/lib/components/form/TextArea.svelte
+++ src/lib/components/form/TextArea.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { VariantProps } from 'class-variance-authority';
-	import type { inputStyles } from './text-styles';
+	import { inputStyles } from './text-styles';
+	import { _ } from 'svelte-i18n';
 
 	export let label: string = '';
 	export let placeholder: string = '';
@@ -14,6 +15,21 @@
 	{#if Boolean(label)}
 		<label class="label-text" for={id}>{label}</label>
 	{/if}
-	<textarea {placeholder} class="w-full " bind:this={element} name={id} {id} cols="30" rows="10" />
-	<span>{error}</span>
+	<textarea
+		{placeholder}
+		class={`w-full ${inputStyles({
+			intent: !!error ? 'error' : variants.intent,
+			height: 'textarea'
+		})}`}
+		bind:this={element}
+		name={id}
+		{id}
+		cols="30"
+		rows="10"
+	/>
+	{#if !!error}
+		<span class="text-error font-bold text-xs mb-2 h-2" data-testid={`${id}-error`}>
+			{$_(error)}
+		</span>
+	{/if}
 </div>
--- src/lib/components/form/text-styles.ts
+++ src/lib/components/form/text-styles.ts
@@ -1,19 +1,24 @@
 import { cva } from 'class-variance-authority';
 
 export const inputStyles = cva(
-	'focus:outline-none input p-2 h-[2.5rem] rounded-md placeholder:text-base-content/40 placeholder:text-sm w-full text-sm',
+	'focus:outline-none input p-2 rounded-md placeholder:text-base-content/40 placeholder:text-sm w-full text-sm',
 	{
 		variants: {
 			intent: {
 				primary: 'border bg-base-200 focus:border-base-content/40',
 				darker: 'bg-gray-200 dark:bg-gray-900',
-				error: 'border-error border-2',
+				error: 'border-error border-2 bg-base-200',
 				searchBar:
 					'border bg-gray-200 dark:bg-gray-900 focus:border-base-content/40 rounded-3xl pl-5 	placeholder:text-lg text-lg h-[3rem]'
+			},
+			height: {
+				default: 'h-[2.5rem]',
+				textarea: 'h-[10rem]'
 			}
 		},
 		defaultVariants: {
-			intent: 'primary'
+			intent: 'primary',
+			height: 'default'
 		}
 	}
 );
